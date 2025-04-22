07a0fb229eb19c65c10191df0943683134bd9078
Abdelrahman Sabry <abdelrahmansabry53@gmail.com>
2025-04-23 01:26:08 +0200

    Media static files4

--- project/settings.py
+++ project/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'project/static')
 ]
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
